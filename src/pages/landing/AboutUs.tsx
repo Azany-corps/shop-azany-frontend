a579895b1aad57097d86b2aaf14f556829126e58
Oracle247 <oebiyeladouemmanuel@gmail.com>
2023-12-18 18:53:31 +0100

    resposive

--- src/pages/landing/AboutUs.tsx
+++ src/pages/landing/AboutUs.tsx
@@ -9,6 +9,7 @@ import Img3 from '../../assets/image.png'
 import Logo from '../../assets/azanylogofinal 2.png'
 
 import { Icon } from '@iconify/react';
+import MobileHeader from '../../components/General/MobileHeader'
 
 const AboutUs = () => {
     const howTos = [
@@ -41,9 +42,11 @@ const AboutUs = () => {
     return (
         <div className='bg-[#eeeeee]'>
             <Header />
+            <MobileHeader />
+
             <div className="p-4">
-                <div className="flex justify-center gap-10 px-24 py-20 items-start rounded-xl bg-black text-white">
-                    <div className="flex flex-col gap-5 text-left items-start w-1/3">
+                <div className="flex md:flex-row flex-col-reverse justify-center gap-6 px-6 py-5 md:gap-10 md:px-24 md:py-20 items-center md:items-start rounded-xl bg-black text-white">
+                    <div className="flex flex-col gap-5 text-left items-start md:w-1/3">
                         <h2 className=' font-semibold text-2xl'>A Family That Keeps On Growing</h2>
                         <p className='text-xs'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum facilisis risus eu vulputate. Sed consectetur tristique diam a posuere. Donec aliquam interdum sollicitudin. Integer porttitor massa sapien, tincidunt</p>
                         <p className='text-xs'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum facilisis risus eu vulputate. Sed consectetur tristique diam a posuere. Donec aliquam interdum sollicitudin. Integer porttitor massa sapien, tincidunt</p>
@@ -54,11 +57,11 @@ const AboutUs = () => {
                 </div>
             </div>
 
-            <div className="flex relative px-4 gap-9 py-6">
-                <div className="flex h-80 w-2/5 rounded-xl overflow-hidden">
+            <div className="flex md:flex-row flex-col relative px-4 md:gap-9 gap-4 py-6">
+                <div className="flex h-80 md:w-2/5 rounded-xl overflow-hidden">
                     <img className='h-full w-full object-cover' src={Img1} alt="" />
                 </div>
-                <div className="flex items-start w-3/5 gap-6 py-6 flex-col">
+                <div className="flex items-start md:w-3/5 gap-6 py-6 flex-col">
                     <div className="flex gap-4 items-end">
                         <span>
                             <img className='h-12' src={Logo} alt="" />
@@ -71,17 +74,17 @@ const AboutUs = () => {
                 </div>
             </div>
 
-            <div className="flex relative px-4 gap-9 py-6">
-                <div className="flex bg-[#EEF9F2] gap-9 p-7">
-                    <div className="flex h-80 w-2/5 rounded-xl overflow-hidden">
+            <div className="flex relative px-4 py-6">
+                <div className="flex md:flex-row flex-col bg-[#EEF9F2] md:gap-9 gap-4 p-7">
+                    <div className="flex h-80 md:w-2/5 rounded-xl overflow-hidden">
                         <img className='h-full w-full object-cover' src={Img2} alt="" />
                     </div>
-                    <div className="flex items-start w-3/5 gap-6 py-6 flex-col">
+                    <div className="flex items-start nd:w-3/5 gap-6 py-6 flex-col">
                         <div className="flex gap-4 items-end">
                             <span className='bg-[#0156FF] rounded-t-full rounded-l-full p-3'>
                                 <Icon icon="ic:round-star" color="white" />
                             </span>
-                            <span className='font-bold text-4xl'>The Highest Quality of Products</span>
+                            <span className='font-bold text-3xl md:text-4xl'>The Highest Quality of Products</span>
                         </div>
                         <p className=''>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum facilisis risus eu vulputate. Sed consectetur tristique diam a posuere. Donec aliquam interdum sollicitudin. Integer porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum facilisis risus eu vulputate. Sed consectetur tristique diam a posuere. Donec e diam a posuere. Donec aliquam interdum sollicitudin. Integer porttitor.
@@ -95,21 +98,21 @@ const AboutUs = () => {
                     <span className='bg-[#0156FF] rounded-t-full rounded-l-full p-3'>
                         <Icon icon="mdi:truck-delivery" color="white" height={35} />
                     </span>
-                    <span className='font-medium text-[50px]'>The Highest Quality of Products</span>
+                    <span className='font-medium text-4xl md:text-[50px]'>We Deliver to Any Regions</span>
                 </div>
-                <p className='text-2xl text-[#231F20] text-center w-3/5'>
+                <p className='text-xl text-[#231F20] text-center md:w-3/5'>
                     We deliver our goods all across Nigeria. No matter where you live, your order will be shipped in time and delivered right to your door or to any other location you have stated. The packages are handled with utmost care, so the ordered products will be handed to you safe and sound, just like you expect them to be
                 </p>
             </div>
 
-            <div className="flex bg-[#470505] px-28 py-10 text-white justify-between items-center">
-                <div className="flex flex-col gap-5 items-start">
+            <div className="flex relative bg-[#470505] md:px-28 px-6 py-10 text-white justify-between items-center">
+                <div className="flex flex-col gap-5 items-center z-10 md:items-start">
                     <h2 className="font-bold text-3xl">We are expanding</h2>
                     <p className=''>Be among the first to expand to our emerging marketplaces</p>
                     <button className='py-4 px-8 rounded-[36px] bg-[#DB4444]'>Start Selling</button>
                 </div>
-                <div className="flex">
-                    <img src={Img3} alt="" className="h-full w-full object-cover" />
+                <div className="flex absolute scale-[0.8] bottom-0 md:relative">
+                    <img src={Img3} alt="" className=" w-full object-cover" />
                 </div>
             </div>
             <Footer />
--- src/pages/landing/RewardGloPoint.tsx
+++ src/pages/landing/RewardGloPoint.tsx
@@ -7,6 +7,7 @@ import Img2 from '../../assets/Group 112.png'
 import Img3 from '../../assets/image.png'
 
 import { Icon } from '@iconify/react';
+import MobileHeader from '../../components/General/MobileHeader'
 
 const RewardGloPoint = () => {
     const howTos = [
@@ -39,12 +40,13 @@ const RewardGloPoint = () => {
     return (
         <div className='bg-[#eeeeee]'>
             <Header />
+            <MobileHeader />
             <div className="flex p-7">
                 <Hero />
-            </div>
+                M</div>
             <div className="px-4">
-                <div className="flex justify-between gap-6 px-24 py-20 items-start bg-black text-white">
-                    <div className="flex flex-col gap-5 text-left items-start w-1/3">
+                <div className="flex flex-col-reverse md:flex-row justify-between gap-6 px-6 py-5 md:px-24 md:py-20 item-center md:items-start bg-black text-white">
+                    <div className="flex flex-col gap-5 text-left items-start md:w-1/3">
                         <h2 className=' font-semibold text-2xl'>What is RewardGlo Point</h2>
                         <p className='text-xs'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum facilisis risus eu vulputate. Sed consectetur tristique diam a posuere. Donec aliquam interdum sollicitudin. Integer porttitor massa sapien, tincidunt</p>
                         <p className='text-xs'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum facilisis risus eu vulputate. Sed consectetur tristique diam a posuere. Donec aliquam interdum sollicitudin. Integer porttitor massa sapien, tincidunt</p>
@@ -59,7 +61,7 @@ const RewardGloPoint = () => {
                     <h2 className="font-bold text-3xl ">How to get reward Point</h2>
                     <p className="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
-                <div className="flex items-center gap-8 px-20">
+                <div className="flex flex-wrap items-center gap-4 md:gap-8 md:px-20 px-5">
                     {
                         howTos.map((item, index) => (
                             <div className="flex flex-col gap-6 items-center">
@@ -73,53 +75,53 @@ const RewardGloPoint = () => {
                     }
                 </div>
             </div>
-            <div className="flex p-7 gap-32 justify-between items-center">
+            <div className="flex md:flex-row flex-col p-7 gap-6 md:gap-32 justify-between items-center">
                 <div className="flex">
                     <img className='w-full h-full object-cover' src={Img2} alt="" />
                 </div>
-                <div className="flex flex-col w-3/5 gap-14">
-                    <h2 className='font-bold text-4xl w-2/3'>What you can use it for</h2>
-                    <div className="grid grid-cols-2 gap-16 gap-y-12">
-                        <div className="flex items-start gap-3 flex-col">
+                <div className="flex flex-col md:w-3/5 gap-14">
+                    <h2 className='font-bold text-4xl md:w-2/3'>What you can use it for</h2>
+                    <div className="grid md:grid-cols-2 place-items-center grid-cols-1 md:gap-16 md:gap-y-12 gap-6">
+                        <div className="flex items-center md:items-start gap-3 flex-col">
                             <span className='bg-[#7ED09C] p-5 mb-3 rounded-2xl'>
                                 <Icon icon="ic:baseline-shopping-bag" />
                             </span>
                             <h2 className='font-semibold text-xl'>Pay for Shopping</h2>
-                            <p className='text-[#606176] w-4/5'>The only costs are for standard school supplies and voluntary field trips.</p>
+                            <p className='text-[#606176] text-center md:text-left md:w-4/5'>The only costs are for standard school supplies and voluntary field trips.</p>
                         </div>
-                        <div className="flex items-start gap-3 flex-col">
+                        <div className="flex items-center md:items-start gap-3 flex-col">
                             <span className='bg-[#7ED09C] p-5 mb-3 rounded-2xl'>
                                 <Icon icon="ic:baseline-shopping-bag" />
                             </span>
                             <h2 className='font-semibold text-xl'>Recharge Card</h2>
-                            <p className='text-[#606176] w-4/5'>Many of our Connections Academy schools have additional accreditations.</p>
+                            <p className='text-[#606176] text-center md:text-left md:w-4/5'>Many of our Connections Academy schools have additional accreditations.</p>
                         </div>
-                        <div className="flex items-start gap-3 flex-col">
+                        <div className="flex items-center md:items-start gap-3 flex-col">
                             <span className='bg-[#7ED09C] p-5 mb-3 rounded-2xl'>
                                 <Icon icon="ic:baseline-shopping-bag" />
                             </span>
                             <h2 className='font-semibold text-xl'>Online curriculum</h2>
-                            <p className='text-[#606176] w-4/5'>Our curriculum prepares students to go further in life by giving them support.</p>
+                            <p className='text-[#606176] text-center md:text-left md:w-4/5'>Our curriculum prepares students to go further in life by giving them support.</p>
                         </div>
-                        <div className="flex items-start gap-3 flex-col">
+                        <div className="flex items-center md:items-start gap-3 flex-col">
                             <span className='bg-[#7ED09C] p-5 mb-3 rounded-2xl'>
                                 <Icon icon="ic:baseline-shopping-bag" />
                             </span>
                             <h2 className='font-semibold text-xl'>Social Interaction</h2>
-                            <p className='text-[#606176] w-4/5'>Students at Connections Academy collaborate on projects together.</p>
+                            <p className='text-[#606176] text-center md:text-left md:w-4/5'>Students at Connections Academy collaborate on projects together.</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className="flex bg-[#470505] px-28 py-10 text-white justify-between items-center">
-                <div className="flex flex-col gap-5 items-start">
+            <div className="flex relative bg-[#470505] md:px-28 px-6 py-10 text-white justify-between items-center">
+                <div className="flex flex-col gap-5 items-center z-10 md:items-start">
                     <h2 className="font-bold text-3xl">We are expanding</h2>
                     <p className=''>Be among the first to expand to our emerging marketplaces</p>
                     <button className='py-4 px-8 rounded-[36px] bg-[#DB4444]'>Start Selling</button>
                 </div>
-                <div className="flex">
-                    <img src={Img3} alt="" className="h-full w-full object-cover" />
+                <div className="flex absolute scale-[0.8] bottom-0 md:relative">
+                    <img src={Img3} alt="" className=" w-full object-cover" />
                 </div>
             </div>
             <Footer />
