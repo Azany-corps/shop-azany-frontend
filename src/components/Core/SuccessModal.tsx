ecfa272c6796a545b07831c1203dec45fca40d93
Mac <mac@Macs-MacBook-Air.local>
2024-01-05 15:14:45 +0100

    changes to success modal check sign

--- src/components/Core/SuccessModal.tsx
+++ src/components/Core/SuccessModal.tsx
@@ -25,18 +25,12 @@ const SuccessModal: React.FC<SuccessModalProps> = ({
       <div className="relative p-4 w-full max-w-[590px] max-h-[500px]">
         <div className="relative bg-white rounded-[26.7px] shadow px-[47px]">
           <div className="px-4 md:px-5 space-y-4 flex flex-col justify-between items-center pt-[90px]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="253"
-              height="253"
-              viewBox="0 0 253 253"
-              fill="none"
-            >
-              <path
-                d="M185.973 21.8191C187.592 18.9926 186.62 15.3749 183.721 13.889C179.378 11.663 174.909 9.69094 170.338 7.98263C167.286 6.84225 163.958 8.56206 162.962 11.6637C161.965 14.7653 163.678 18.073 166.724 19.228C170.517 20.6661 174.23 22.3049 177.848 24.1374C180.755 25.6094 184.353 24.6457 185.973 21.8191Z"
-                fill="#00A91B"
+            <div className="w-[252px] h-[252px]">
+              <img
+                src="/images/success-green-check-mark-icon.svg"
+                alt="success"
               />
-            </svg>
+            </div>
             <p className="text-center text-[17px] z-20 font-semibold mt-[34px] pb-[85px]">
               {title || "Your password has been changed"}
             </p>
